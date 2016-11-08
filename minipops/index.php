7fff495b3024a9834d4e9bdb223b57c4d271f5ec
stephendltg <s.deletang@laposte.net>
2016-11-08 19:19:54 +0100

    amélioration index.php

--- minipops/index.php
+++ minipops/index.php
@@ -1,26 +1,16 @@
 <?php
 /**
+ * Front to the Minipops application.
  *
  * @package CMS mini POPS
  */
 
+
 /** On definit le repertoire racine  */
 define( 'ABSPATH', dirname(__FILE__) . '/' );
 
-// On définit le coeur du CMS
-define( 'INC', 'core' );
+/** On charge le coeur de minipops  */
+require_once( ABSPATH . 'core/mp_load.php');
 
-/** On verifie que le fichier config existe  */
-if( file_exists( ABSPATH . 'mp-config.php') ) {
-
-	/** Le fichier config est dans  ABSPATH */
-	require_once( ABSPATH . 'mp-config.php' );
-
-} elseif( @file_exists( dirname( ABSPATH ) . '/mp-config.php' ) ){
-
-	/** Le fichier config est un cran au dessus de ABSPATH */
-	require_once( dirname( ABSPATH ) . '/mp-config.php' );
-}
-
-require_once(ABSPATH . INC . '/mp_load.php');
-require_once(ABSPATH . INC . '/mp_template_load.php');
+/** On charge la gestion des templates */
+require_once( ABSPATH . 'core/mp_template_load.php');
